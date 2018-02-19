460a6e34be6f5c2e438760122aa912ed477badfb
App Academy Student <student@appacademy.io>
2018-02-19 14:49:38 -0800

    Create Board class and dummy piece class

---
+++ W2D1/Chess/board.rb
@@ -0,0 +1,40 @@
+require_relative "piece"
+
+class Board
+  attr_accessor :grid
+
+
+  def initialize
+    @grid = Array.new(8) { Array.new(8) }
+    create_pieces
+  end
+
+  def create_pieces
+    self.grid[0].map! { |_| Piece.new }
+    self.grid[-1].map! { |_| Piece.new }
+  end
+
+  def empty?(pos)
+    self[pos] == nil
+  end
+
+  def valid_move?(start_pos, end_pos)
+    !empty?(start_pos)
+  end
+
+  def move_piece(start_pos, end_pos)
+    raise ArgumentError unless valid_move?(start_pos, end_pos)
+    self[start_pos], self[end_pos] = nil, self[start_pos]
+  end
+
+  def []=(pos, piece)
+    row, col = pos
+    self.grid[row][col] = piece
+  end
+
+  def [](pos)
+    row, col = pos
+    self.grid[row][col]
+  end
+
+end
